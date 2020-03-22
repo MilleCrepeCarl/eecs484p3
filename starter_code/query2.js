ee4ac0ce7a4621adb887bf6c1e762406597fde2c
wucarl <wucarl@umich.edu>
2020-03-21 23:50:33 -0400

    q2

--- starter_code/query2.js
+++ starter_code/query2.js
@@ -1,4 +1,4 @@
-// query2 : unwind friends and create a collection called 'flat_users' 
+// query2 : unwind friends and create a collection called 'flat_users'
 // where each document has the following schema:
 /*
 {
@@ -10,7 +10,7 @@
 function unwind_friends(dbname){
     db = db.getSiblingDB(dbname);
     // TODO: unwind friends
+    db.users.aggregate( [ { $unwind: "$friends" }, { $project : { _id : 0, user_id : 1 , friends : 1 } }, { $out : "flat_users" } ] );
 
-    
     // returns nothing. It creates a collection instead as specified above.
 }
